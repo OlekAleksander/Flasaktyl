3e160b73da944b9aed6ef3ced25b2fcbbe282ebe
OlekAleksander <66617078+OlekAleksander@users.noreply.github.com>
2022-06-07 16:02:25 +0200

    update logger.py

--- logger.py
+++ logger.py
@@ -1,7 +1,7 @@
 try:
    from colorama import Fore, Back, Style
 
-def log(message,type):
+def log(message):
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     print(current_time + "| " + message)
