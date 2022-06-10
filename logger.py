983a11f1b9bae9e88c4ae12f49733dcb7d11751f
OlekAleksander <66617078+OlekAleksander@users.noreply.github.com>
2022-06-10 19:53:43 +0200

    Delete logger.py

--- logger.py
+++
@@ -1,6 +0,0 @@
-from colorama import Fore, Back, Style
-from datetime import datetime
-def log(message):
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    print(current_time + ": " + message)
